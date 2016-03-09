2c0d126abbbd0ec8fb84e97807db211e7af2c9c4
maxbert <mbertfield@stuy.edu>
2016-03-09 14:50:54 -0500

    its working now

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -24,8 +24,39 @@ public class MergeSortTester
      *  YOUR APPARATUS FOR GENERATING EXECUTION 
      *  TIME DATA...>
      ******************************/
+
+   
     public static void main( String[] args ) 
     {
+	System.out.println("test");
+	int[] a = new int[1000000];
+	int[] b = new int[10000000];
+	int[] c = new int[100000000];
+	for (int i = 0; i < a.length; i++){
+	    int x = (int)(Math.random()  * 100);
+	    a[i] = x;
+	}
+	for (int i = 0; i < b.length; i++){
+	    int x = (int)(Math.random()  * 100);
+	    b[i] = x;
+	}
+	for (int i = 0; i < c.length; i++){
+	    int x = (int)(Math.random()  * 100);
+	    c[i] = x;
+	}
+	long start = System.currentTimeMillis();
+	MergeSort.sort(a);
+	long end = System.currentTimeMillis();
+	System.out.println(" 10 took" +( end- start) + "seconds!");
+        start = System.currentTimeMillis();
+	MergeSort.sort(b);
+	end = System.currentTimeMillis();
+	System.out.println(" 100 took" +( end- start) + "seconds!");
+	start = System.currentTimeMillis();
+	MergeSort.sort(c);
+	end = System.currentTimeMillis();
+	System.out.println(" 1000 took" +( end- start) + "seconds!");
+
 
     }//end main
 
